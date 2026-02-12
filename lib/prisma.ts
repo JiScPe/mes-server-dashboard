9c31a3fd34f9f04a1fbcd5cde136c5f2575a5b3f
JiScPe <jetsada.pra@hotmail.com>
2026-02-12 14:40:16 +0700

    add throw error for load env

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,17 +2,25 @@ import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+  prisma: PrismaClient | undefined
+}
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
 }
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
