0e7e40b0241656d13482558703e91f7d14241623
JiScPe <jetsada.pra@hotmail.com>
2026-02-12 13:45:11 +0700

    fix build error remove ipaas

--- app/api/ipaas/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const ipass = await prisma.ipaas.findMany();
-  return NextResponse.json(ipass);
-}
